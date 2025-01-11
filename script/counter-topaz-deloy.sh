9ee874060c43c2a2a9c146ef9800b21de4d3ef0e
Mark Vujevits <vujevits.mark@gmail.com>
2025-01-11 10:28:42 +0100

    add both rpc urls

--- script/counter-topaz-deloy.sh
+++ script/counter-topaz-deloy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 source .env
+# RPC_URL="https://rpc.topaz.treasure.lol"
+RPC_URL="https://sepolia.era.zksync.dev"
 forge script --zksync script/Counter.s.sol:CounterScript \
-  --rpc-url https://sepolia.era.zksync.dev \
+  --rpc-url $RPC_URL \
   --broadcast \
   --private-key $PRIVATE_KEY
